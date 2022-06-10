26ecd3551c861de3183865bf06a443f8f23423df
Maax <massimiliano.giordanoorsini001@studenti.uniparthenope.it>
2022-06-10 15:42:58 +0200

    CHANGES:
    - first test

--- Source/matxvet.c
+++ Source/matxvet.c
@@ -26,8 +26,50 @@ L'algoritmo è sviluppato in ambiente MPI_DOCKER.
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <time.h>
+
 //#include "mpi.h"
 #define MAXBUF 1024
+
+// read rows
+// read cols
+// read elements from text file
+void generateMatrix(int rows, int cols) {
+
+    FILE *text = fopen(argv[1], "w+");
+
+    fseek(text, 0, SEEK_SET);
+    fprintf(text, "%d\n", rows);
+    fprintf(text, "%d\n", cols);
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            fprintf(text, "%d\n", (int) rand()%587);
+        }
+    }
+    for (int i = 0; i < cols; i++) {
+        fprintf(text, "%d\n", (int) rand()%345);
+    }
+
+}
+
+int **readMatrix(FILE *text, int *rows, int *cols)
+{
+}
+
+
+void createGrid(MPI_Comm *grid, int processorID, int numberOfProcessors, int rows, int cols) {
+    int dim = 2, *ndim, reorder = 0, *period;
+
+    ndim = (int*) calloc(dim, sizeof(int));
+    // qxp 
+    
+
+}
+
+void matXvet_local(MPI_Comm* grid, int processorID, int numberOfProcessor, int nloc, int cols, int* b) {
+
+}
 
 // ./matxvet <textfilename>
 int main(int argc, char *argv[])
@@ -36,16 +78,66 @@ int main(int argc, char *argv[])
     int byteRead;
     char buffer[MAXBUF];
     int processorID, numberOfProcessor;
+    MPI_Status status;
+    MPI_Comm grid;
+    int nloc, q, p, mod;
+    int *xloc;
+    // Ax = y
+    
+    generateMatrix(atoi(argv[2]), atoi(argv[3]));
+    
+    // read from textfile
+    FILE *text = fopen(argv[1], "r");
+    fseek(text, 0, SEEK_SET);
+    fscanf(text, "%d", &rows);
+    fscanf(text, "%d", &cols);
 
-    FILE *text_fd = fopen("input_file.txt", "r");
-    fseek(text_fd, 0, SEEK_SET);
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &processorID);
+    MPI_Comm_size(MPI_COMM_WORLD, &numberOfProcessor);
 
-    fscanf(text_fd, "%d", &rows);
+    int *A = (int*)malloc(rows * cols * sizeof(int));
+    int *x = (int*) malloc(cols * sizeof(int));
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            fscanf(text, "%d", &A[rows * i + j]);
+        }
+    }
 
-    //rows = atoi(buffer);
+    // lettura del vettore b
+    for (int i = 0; i < cols; i++) {
+        fscanf(text, "%d", &x[i]);
+    }
 
-    printf("buffer: %s\n", buffer);
-    printf("number: %d\n", rows);
+   
+
+
+    /* DISTRIBUZIONE DEI DATI */
+
+    // invio dimensioni matrice
+    MPI_Bcast();
+
+    // mod(M,p) != 0 -> OK
+    // mod(N,q) != 0 -> ridistribuire il resto delle righe
+
+    if ((mod = rows/q) == 0) nloc = rows/q;
+    else {
+        for (int step = 0; step < mod; step++)
+            nloc++;
+    }
+    xloc = (int*) malloc(nloc*sizeof(int));
+
+
+    MPI_Scatter(A, nloc, MPI_INT, xloc, nloc, MPI_INT, 0, /* grid */ MPI_COMM_WORLD);
+
+    // invio dimensioni vettore
+    MPI_Bcast();
+
+
+
+    /* CALCOLO LOCALE */
+    matXvet_local(/* &grid */, processorID, numberOfProcessor, nloc, cols/p, xloc, x)
+
 
     return 0;
 }
